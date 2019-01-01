c83fb9a375a9cb98a0547a7e6b2d8e38f129e4c7
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-01 14:08:37 -0800

    Initial Commit

---
+++ Kata9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Kata9
+//
+//  Created by jason harrison on 2019-01-01.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
